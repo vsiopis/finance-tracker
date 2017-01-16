e2c27531e319b7d90dc6250b0701637a7db5acdc
vsiopis <vsiopi@gmail.com>
2017-01-16 21:27:32 +0200

    Corrent key names

--- app/controllers/user/registrations_controller.rb
+++ app/controllers/user/registrations_controller.rb
@@ -5,9 +5,7 @@ class User::RegistrationsController < Devise::RegistrationsController
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_namae, :last_name])
-   # devise_parameter_sanitizer.for(:sign_up).push(:first_name, :last_name)
-   # devise_parameter_sanititer.for(:account_update).push(:first_name, :last_name) 
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
   end
 
 end
